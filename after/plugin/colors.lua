fa97c7653301d8ffad6e906b7df9eee9023dea44
Thomas Rayner <thmsrynr@outlook.com>
2024-01-19 12:04:26 -0800

    fix autocomplete menu bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,6 +3,7 @@ function setcolors(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.cmd("hi normal guibg=NONE")
+    vim.cmd("hi Pmenu ctermbg=10 ctermfg=15 guibg=#BBBBBB guifg=#000000")
 end
 
 setcolors()

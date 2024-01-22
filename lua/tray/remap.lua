01e72424392e7f8691b887a57356fb4856a64c4f
Thomas Rayner <thmsrynr@outlook.com>
2024-01-22 13:42:00 -0800

    shortcut descriptions

--- lua/tray/remap.lua
+++ lua/tray/remap.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc="File explorer"})
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "J", ":move -2<CR>")
-vim.keymap.set("n", "K", ":move +1<CR>")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc="Move line down"})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc="Move line up"})
+vim.keymap.set("n", "J", ":move -2<CR>", {desc="Move line up"})
+vim.keymap.set("n", "K", ":move +1<CR>", {desc="Move line down"})
 
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>p", [["+p]], {desc="Paste from clipboard"})
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], {desc="Copy to clipboard"})

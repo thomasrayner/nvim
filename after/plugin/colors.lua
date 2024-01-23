dad7ab4bb57fd3069516194ae780b1e0ff5d0646
Thomas Rayner <thmsrynr@outlook.com>
2024-01-23 15:06:07 -0800

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,5 @@
 function setcolors(color) 
     vim.cmd("colorscheme base16-onedark")
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.cmd("hi normal guibg=NONE")
     vim.cmd("hi Pmenu ctermbg=10 ctermfg=15 guibg=#BBBBBB guifg=#000000")
 end
 
